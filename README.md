36ae848708f0b4c6409322f8a3eb8ffb64de555e
NaomiRan <naomiran1989@gmail.com>
2024-09-21 12:17:04 -0400

    content rating project

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # coding-project-template
+# 1. boiling template for this project and create a foleder "content_rating".
+git clone https://github.com/ibm-developer-skills-network/content_rating.git
+# 2. go nside the folder "content_rating".
+cd content_rating
+# 3.  install all the necessary packages to execute the application.
+npm install
+# 4.run the application, providing you with port number 5173.
+npm run dev
+
+# Or using those command to clone a git repository: 
+npx degit user/project#main my-project-name   e.g   npx degit ibm-developer-skills-network/content_rating#main content_rating
+cd content_rating
+
+npm install
+npm run dev
+
+# 5.
--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -5,11 +5,47 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
+    /* Initialize the counters*/
+    this.state = {
+      likes: 0,
+      dislikes: 0,
+      totalRatings:0,
+      /*events to handle the clicks*/
+      handleLike: () => {
+        this.setState((prevState) => ({
+          likes: prevState.likes + 1,
+          totalRatings: prevState.totalRatings + 1
+        }));
+      },
+
+      handleDislike: () => {
+        this.setState((prevState) => ({
+          dislikes: prevState.dislikes + 1,
+          totalRatings: prevState.totalRatings + 1
+        }));
+      }
+    };
   }
+
   render() {
     return (
      <>
-     <h1>Text Content Rating</h1>
+        <h1>Text Content Rating</h1>
+        <div className='content-rating'>
+          <p>
+            The app will count the rating by clicking like and dislike button.
+          </p>
+          <p>Click Like button if you like this app!</p>
+          <div className='rating-buttons'>
+            <button className='like-button' onClick={this.state.handleLike}>
+              Like ({this.state.likes})
+            </button>
+            <button className='dislike-button' onClick={this.state.handleDislike}>
+              Dislike ({this.state.dislikes})
+            </button>
+          </div>
+          <p>Total Ratings: {this.state.totalRatings}</p>
+      </div>
      </>
     );
   }
